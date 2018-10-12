ca3df6838c924de0b0ca798a41b2ce90305fc7ff
florian <florian_val@outlook.fr>
2018-10-12 09:19:31 +0200

    initial commit

---
+++ src/projet/Projet.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author florianvalade
+ */
+public class Projet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        RendezVous rdv1 = new RendezVous("17/03/2018","17:00","17:30");
+
+    }
+    
+}
---
+++ src/projet/RendezVous.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet;
+import java.util.Date;
+/**
+ *
+ * @author florianvalade
+ */
+public class RendezVous {
+    private String date;
+    private String h_start;
+    private String h_end;
+    private String label;
+    private boolean reminder;
+    
+    public RendezVous(String date, String h_start, String h_end) {
+        // check if date is valid
+        if (date.split("/")[0] != date || date.length() != 10)
+        {
+            Date date_now = new Date();
+            int day = Integer.parseInt(date.split("/")[0]);
+            int month = Integer.parseInt(date.split("/")[1]) - 1;
+            int year = Integer.parseInt(date.split("/")[2]) - 1900;
+            Date date_given = new Date(year, month, day);
+            System.out.print(date_given);
+            System.out.print("\n");
+            if (date_now.after(date_given) == true){            
+                System.out.print("Date given is before today's date");
+            }
+        }
+        else
+        {
+            System.out.print("Problem creating RendezVous please use date format jj/mm/aaaa");
+        }
+        this.date = date;
+        this.h_start = h_start;
+        this.h_end = h_end;
+    }
+    
+    public RendezVous(String date, String h_start, String h_end, boolean reminder) {
+        this(date, h_start, h_end);
+        this.reminder = reminder;
+    }
+
+    public RendezVous(String date, String h_start, String h_end, String label, boolean reminder) {
+        this(date, h_start, h_end, reminder);
+        this.label = label;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getH_start() {
+        return h_start;
+    }
+
+    public void setH_start(String h_start) {
+        this.h_start = h_start;
+    }
+
+    public String getH_end() {
+        return h_end;
+    }
+
+    public void setH_end(String h_end) {
+        this.h_end = h_end;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+    
+    
+
+
+}
