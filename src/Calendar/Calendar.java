c7fd5b731661cc778a147aa1b2a97dd964cb12df
florian <florian_val@outlook.fr>
2018-11-20 17:41:03 +0100

    menu to edit rdv

--- src/Calendar/Calendar.java
+++ src/Calendar/Calendar.java
@@ -78,12 +78,11 @@ public class Calendar {
                                 //list rdvs of agenda
                                 get_print_rdvs(agendas.get(i));
                                 agenda_number = i;
+                                break;
                         }}
                         //temporary then go in rdv menu
                         if (agenda_number != -1){
                             status = menu_rdv(agendas.get(agenda_number).getRdv());}
-                        System.out.println(choix);
-                        System.out.println(status);
                         choix = 0;
                         break;
                     case 3:
@@ -120,6 +119,21 @@ public class Calendar {
                     new_rdv = new RendezVous(date,h_start,h_end);
                     rdv.add(new_rdv);
                     break;
+                case 2: 
+                    int rdv_to_edit;
+                    System.out.println("Quel rdv voulez vous changer ?");
+                    for (int i =0; i < rdv.size(); i++){
+                        System.out.print(i + 1);
+                        System.out.print(" - ");
+                        System.out.println(rdv.get(i).toString());
+                    }
+                    rdv_to_edit =  scan.nextInt();
+                    if(rdv_to_edit < rdv.size() && rdv_to_edit >0){
+                        System.out.print("changement sur : ");
+                        System.out.println(rdv.get(rdv_to_edit +1));
+                        edit_rdv(rdv.get(rdv_to_edit +1));
+                    }
+                    break;
                 case 3:
                     status = 1;
                     break;
@@ -127,6 +141,13 @@ public class Calendar {
         }
         return status;
     }
+    
+    public static void edit_rdv(RendezVous rdv){
+    //menu to choose what to changes
+    
+    
+    }
+    
     public static List<RendezVous> get_print_rdvs(Agenda agenda){
         List<RendezVous> rdv_agenda;
         rdv_agenda = agenda.getRdv();
